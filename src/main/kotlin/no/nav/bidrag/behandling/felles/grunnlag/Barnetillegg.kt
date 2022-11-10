caf1cfbc800ab190295355f08cf5876ad88444a9
Lars Otto Haugen <lars.otto.haugen@nav.no>
2022-11-10 15:35:07 +0100

    Lagt til kilde på Barnetillegg-objektet

--- src/main/kotlin/no/nav/bidrag/behandling/felles/grunnlag/Barnetillegg.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/felles/grunnlag/Barnetillegg.kt
@@ -9,13 +9,14 @@ class Barnetillegg(
   override val soknadsbarnId: Int,
   val bruttoBelop: BigDecimal,
   val skattProsent: BigDecimal,
+  val kilde: String,
   datoFom: LocalDate,
   datoTil: LocalDate?
 ) :
   Periode<Barnetillegg>(datoFom, datoTil), IGrunnlagInnhold, ISoknadsbarn, IRolle {
 
   override fun isEqualTo(other: Barnetillegg): Boolean {
-    return rolle == other.rolle && soknadsbarnId == other.soknadsbarnId && bruttoBelop == other.bruttoBelop && skattProsent == other.skattProsent
+    return rolle == other.rolle && soknadsbarnId == other.soknadsbarnId && bruttoBelop == other.bruttoBelop && skattProsent == other.skattProsent && kilde == other.kilde
   }
 
 }
